b3b87750cb42c5a3d78e244640663b262cb01273
shehabeldin-mohamed <shehabkh249@gmail.com>
2025-11-29 22:51:24 +0100

    Implemented Adapter pattern

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -5,9 +5,22 @@ namespace DesignPattern.Adapter
     {
         private readonly BillingSystem thirdPartyBillingSystem = new();
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            var employeeList = new List<Employee>();
+            var rowCount = employeesArray.GetLength(0);
+
+            for (var i = 0; i < rowCount; i++)
+            {
+                var idStr = employeesArray[i, 0];
+                var name = employeesArray[i, 1];
+                var designation = employeesArray[i, 2];
+                var salaryStr = employeesArray[i, 3];
+                
+                var employee = new Employee(int.Parse(idStr), name, designation, decimal.Parse(salaryStr));
+                employeeList.Add(employee);
+            }
+            thirdPartyBillingSystem.ProcessSalary(employeeList);
+        }
     }
 }
