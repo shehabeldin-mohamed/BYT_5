8d2b113bc8558774ab6010926a3ba9c29f16938e
shehabeldin-mohamed <shehabkh249@gmail.com>
2025-11-29 22:26:01 +0100

    Implemented Factort pattern

--- DesignPattern/Factory/DatabaseFactory.cs
+++ DesignPattern/Factory/DatabaseFactory.cs
@@ -2,9 +2,14 @@
 {
     public static class DatabaseFactory
     {
-        //public static IDatabase CreateDatabase(...)
-        //{
-        //  ...
-        //}
+        public static IDatabase CreateDatabase(DatabaseType type)
+        {
+            return type switch
+            {
+                DatabaseType.SqlServer => new SqlServerDatabase(),
+                DatabaseType.Oracle => new OracleDatabase(),
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, "Unsupported database type.")
+            };
+        }
     }
 }
